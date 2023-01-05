464e018855352c6b1b9d9d63ccf0f3623f81bdad
jiangyingjie <jiangyingjie@ingenu.cn>
2023-01-05 15:28:54 +0800

    删除测试

--- v1_指针数组/mystring.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MYSTRING_H_
-#define _MYSTRING_H_
-
-#include <stdio.h>
-
-int mystrcmp(char *, char *);
-
-
-#endif
--- v2_链表/mystring.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MYSTRING_H_
-#define _MYSTRING_H_
-
-// 字符串拷贝
-char *mystrcpy(char *des, char *src);
-// 比较两个字符串的大小
-int mystrcmp(char *str1, char *str2);
-#endif
